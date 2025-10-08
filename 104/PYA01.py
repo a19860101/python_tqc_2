830dee3ed6ce88c313c63078f636e40e0d1b455e
Zac <a19860101@gmail.com>
2025-10-08 19:42:34 +0800

    202

---
+++ 104/PYA01.py
@@ -0,0 +1,38 @@
+# 載入 json 模組
+import json
+
+data = {
+    'people':[
+        {
+            'id': '1',
+            'name': 'Peter',
+            'country': 'Taiwan'
+        },
+        {
+            'id': '2',
+            'name': 'Jack',
+            'country': 'USA',
+        },
+        {
+            'id': '3',
+            'name': 'Cindy',
+            'country': 'Japan',
+        }
+    ]
+}
+# 建立資料
+# 'id': '1'
+# 'name': 'Peter'
+# 'country': 'Taiwan'
+#
+# 'id': '2'
+# 'name': 'Jack'
+# 'country': 'USA'
+#
+# 'id': '3'
+# 'name': 'Cindy'
+# 'country': 'Japan'
+
+# 將資料寫入json檔案
+with open('./write.json', 'w') as outfile:
+    json.dump(data, outfile)
